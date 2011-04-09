8b8e07b58dadedbdac46ff193180319a8fb1e5ff
Jon Seymour <jon.seymour@gmail.com>
2011-04-09 14:57:00 +1000

    Add an example which shows how the write function can be used to duplicate headers.

---
+++ samples/duplicate-headers.js
@@ -0,0 +1,40 @@
+// CSV sample - Copyright jon seymour jon.seymour@gmail.com
+// node samples/duplicate-headers.js < samples/duplicate-headers.in
+
+//
+// This sample shows how the write function can be used
+// to write a new record into the output stream.
+//
+// In this rather contrived example, the first line read
+// is interpreted as a header and is inserted before
+// every other data line in the file.
+//
+
+  var
+    csv = require('csv'),
+    header;
+
+    process.stdin.resume();
+      csv()
+	.fromStream(process.stdin)
+	.toStream(process.stdout)
+	.transform(function(data){
+	  if (header) {
+	    this.write(header);
+	  } else {
+	    header=data;
+	    return null;
+	  }
+	  return data;
+	})
+	.on('error',function(error){
+	  console.log(error.message);
+	});
+
+//
+// expected output
+// 
+//ts,year,ms,chars,age,date
+//20322051544,1979.0,8.8017226E7,ABC,45,2000-01-01
+//ts,year,ms,chars,age,date
+//28392898392,1974.0,8.8392926E7,DEF,23,2050-11-27
