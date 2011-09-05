a0c8f0338562fbdaf1ff43f90cca1497ed8297cf
David Worms <david@adaltas.com>
2011-09-06 00:07:38 +0200

    chore(csv): Fix transform count argument when writing objects or arrays

--- test/write.js
+++ test/write.js
@@ -6,81 +6,81 @@ var fs = require('fs'),
 	csv = require('csv');
 
 module.exports = {
-	// 'Test write array': function(){
-	// 	var count = 0;
-	// 	var test = csv()
-	// 	.toPath(__dirname+'/write/write_array.tmp')
-	// 	.on('data', function(data, index){
-	// 		assert.ok(Array.isArray(data));
-	// 		assert.eql(count,index);
-	// 		count++;
-	// 	})
-	// 	.on('end',function(){
-	// 		assert.eql(1000,count);
-	// 		assert.equal(
-	// 			fs.readFileSync(__dirname+'/write/write.out').toString(),
-	// 			fs.readFileSync(__dirname+'/write/write_array.tmp').toString()
-	// 		);
-	// 		fs.unlink(__dirname+'/write/write_array.tmp');
-	// 	});
-	// 	for(var i=0; i<1000; i++){
-	// 		test.write(['Test '+i,i,'"']);
-	// 	}
-	// 	test.end();
-	// },
-	// 'Test write object with column options': function(){
-	// 	var count = 0;
-	// 	var test = csv()
-	// 	.toPath(__dirname+'/write/write_object.tmp',{
-	// 		columns: ['name','value','escape']
-	// 	})
-	// 	.on('data', function(data, index){
-	// 		assert.ok(typeof data === 'object');
-	// 		assert.ok(!Array.isArray(data));
-	// 		assert.eql(count,index);
-	// 		count++;
-	// 	})
-	// 	.on('end',function(){
-	// 		assert.eql(1000,count);
-	// 		assert.equal(
-	// 			fs.readFileSync(__dirname+'/write/write.out').toString(),
-	// 			fs.readFileSync(__dirname+'/write/write_object.tmp').toString()
-	// 		);
-	// 		fs.unlink(__dirname+'/write/write_object.tmp');
-	// 	});
-	// 	for(var i=0; i<1000; i++){
-	// 		test.write({name: 'Test '+i, value:i, escape: '"', ovni: 'ET '+i});
-	// 	}
-	// 	test.end();
-	// },
-	// 'Test write string': function(){
-	// 	var count = 0;
-	// 	var test = csv()
-	// 	.toPath(__dirname+'/write/write_string.tmp')
-	// 	.on('data', function(data, index){
-	// 		assert.ok(Array.isArray(data));
-	// 		assert.eql(count,index);
-	// 		count++;
-	// 	})
-	// 	.on('end',function(){
-	// 		assert.eql(1000,count);
-	// 		assert.equal(
-	// 			fs.readFileSync(__dirname+'/write/write.out').toString(),
-	// 			fs.readFileSync(__dirname+'/write/write_string.tmp').toString()
-	// 		);
-	// 		fs.unlink(__dirname+'/write/write_string.tmp');
-	// 	});
-	// 	var buffer = '';
-	// 	for(var i=0; i<1000; i++){
-	// 		buffer += ''.concat('Test '+i,',',i,',','""""',"\r");
-	// 		if(buffer.length > 250){
-	// 			test.write(buffer.substr(0,250));
-	// 			buffer = buffer.substr(250);
-	// 		}
-	// 	}
-	// 	test.write(buffer);
-	// 	test.end();
-	// },
+	'Test write array': function(){
+		var count = 0;
+		var test = csv()
+		.toPath(__dirname+'/write/write_array.tmp')
+		.on('data', function(data, index){
+			assert.ok(Array.isArray(data));
+			assert.eql(count,index);
+			count++;
+		})
+		.on('end',function(){
+			assert.eql(1000,count);
+			assert.equal(
+				fs.readFileSync(__dirname+'/write/write.out').toString(),
+				fs.readFileSync(__dirname+'/write/write_array.tmp').toString()
+			);
+			fs.unlinkSync(__dirname+'/write/write_array.tmp');
+		});
+		for(var i=0; i<1000; i++){
+			test.write(['Test '+i,i,'"']);
+		}
+		test.end();
+	},
+	'Test write object with column options': function(){
+		var count = 0;
+		var test = csv()
+		.toPath(__dirname+'/write/write_object.tmp',{
+			columns: ['name','value','escape']
+		})
+		.on('data', function(data, index){
+			assert.ok(typeof data === 'object');
+			assert.ok(!Array.isArray(data));
+			assert.eql(count, index);
+			count++;
+		})
+		.on('end',function(){
+			assert.eql(1000,count);
+			assert.equal(
+				fs.readFileSync(__dirname+'/write/write.out').toString(),
+				fs.readFileSync(__dirname+'/write/write_object.tmp').toString()
+			);
+			fs.unlinkSync(__dirname+'/write/write_object.tmp');
+		});
+		for(var i=0; i<1000; i++){
+			test.write({name: 'Test '+i, value:i, escape: '"', ovni: 'ET '+i});
+		}
+		test.end();
+	},
+	'Test write string': function(){
+		var count = 0;
+		var test = csv()
+		.toPath(__dirname+'/write/write_string.tmp')
+		.on('data', function(data, index){
+			assert.ok(Array.isArray(data));
+			assert.eql(count,index);
+			count++;
+		})
+		.on('end',function(){
+			assert.eql(1000,count);
+			assert.equal(
+				fs.readFileSync(__dirname+'/write/write.out').toString(),
+				fs.readFileSync(__dirname+'/write/write_string.tmp').toString()
+			);
+			fs.unlinkSync(__dirname+'/write/write_string.tmp');
+		});
+		var buffer = '';
+		for(var i=0; i<1000; i++){
+			buffer += ''.concat('Test '+i,',',i,',','""""',"\r");
+			if(buffer.length > 250){
+				test.write(buffer.substr(0,250));
+				buffer = buffer.substr(250);
+			}
+		}
+		test.write(buffer);
+		test.end();
+	},
 	'Test write string with preserve': function(){
 		var count = 0;
 		var test = csv()
@@ -102,7 +102,7 @@ module.exports = {
 				fs.readFileSync(__dirname+'/write/string_preserve.out').toString(),
 				fs.readFileSync(__dirname+'/write/string_preserve.tmp').toString()
 			);
-			fs.unlink(__dirname+'/write/write_string.tmp');
+			fs.unlinkSync(__dirname+'/write/string_preserve.tmp');
 		});
 		test.write('# This line should not be parsed',true);
 		test.write('\n',true);
