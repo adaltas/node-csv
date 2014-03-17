c69a61eb1b5d1d4f7561421de33e8aee7c659a62
David Worms <david@adaltas.com>
2014-03-18 00:16:07 +0100

    chore(csv-parse): Fix callback sample

--- samples/callback.js
+++ samples/callback.js
@@ -3,6 +3,6 @@ should = require('should');
 parse = require('../lib');
 
 input = '#Welcome\n"1","2","3","4"\n"a","b","c","d"';
-parse(input, function(err, output){
+parse(input, {comment: '#'}, function(err, output){
   output.should.eql([ [ '1', '2', '3', '4' ], [ 'a', 'b', 'c', 'd' ] ]);
 });
