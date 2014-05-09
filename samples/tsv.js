4bb9c6b26773a0d675be652b0f027cbcd5978941
David Worms <david@adaltas.com>
2014-05-09 21:28:11 +0200

    add comment, objname and tsv samples

---
+++ samples/tsv.js
@@ -0,0 +1,8 @@
+
+parse = require('..');
+should = require('should');
+
+parse( "1	2	3\ra	b	c", {delimiter: '\t'}, function(err, data){
+  if(err) throw err;
+  data.should.eql([ [ '1', '2', '3' ], [ 'a', 'b', 'c' ] ]);
+});
