3c0f0f9eae7c6aa4cd63ffa6dc1f51e9aeb8f4c3
Mark Stosberg <mark@rideamigos.com>
2014-05-09 08:49:12 -0400

    Document stream.js
    When I first opened this file, it wasn't clear where the input data came from. I wasn't expecting it  at the bottom. I added a few comments to clarify what's happening for new users.

--- samples/stream.js
+++ samples/stream.js
@@ -3,6 +3,9 @@ require('should');
 var parse = require('../lib');
 
 output = [];
+
+// Create a parser as a writable stream
+// Each parsed row is pushed on the output array
 parser = parse({delimiter: ':'})
 parser.on('readable', function(){
   while(row = parser.read()){
@@ -12,12 +15,17 @@ parser.on('readable', function(){
 parser.on('error', function(err){
   consol.log(err.message);
 });
+
+
+// When we are done, test that the parsed output matched what expected
 parser.on('finish', function(){
   output.should.eql([
     [ 'root','x','0','0','root','/root','/bin/bash' ],
     [ 'someone','x','1022','1022','a funny cat','/home/someone','/bin/bash' ]
   ]);
 });
+
+// Now that setup is done, write some data to the stream
 parser.write("root:x:0:0:root:/root:/bin/bash\n");
 parser.write("someone:x:1022:1022:a funny cat:/home/someone:/bin/bash\n");
 parser.end()
