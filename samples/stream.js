1ce750fc0361d511406ad96b540cd1ce2d6e4062
David Worms <david@adaltas.com>
2015-04-22 22:37:00 +0200

    sample: fix #191, stream not closed

--- samples/stream.js
+++ samples/stream.js
@@ -1,9 +1,11 @@
 
 var csv = require('..');
+var i = 0
 
 var generator = csv.generate({seed: 1, columns: 2, length: 20});
 var parser = csv.parse();
 var transformer = csv.transform(function(data){
+  i++
   return data.map(function(value){return value.toUpperCase()});
 });
 var stringifier = csv.stringify();
@@ -13,11 +15,17 @@ generator.on('readable', function(){
     parser.write(data);
   }
 });
+generator.on('end', function(){
+  parser.end()
+});
 
 parser.on('readable', function(){
   while(data = parser.read()){
     transformer.write(data);
   }
+});
+parser.on('end', function(){
+  transformer.end()
 });
 
 transformer.on('readable', function(){
@@ -25,9 +33,17 @@ transformer.on('readable', function(){
     stringifier.write(data);
   }
 });
+transformer.on('end', function(){
+  stringifier.end();
+});
 
 stringifier.on('readable', function(){
   while(data = stringifier.read()){
     process.stdout.write(data);
   }
 });
+generator.on('end', function(){
+  process.stdout.write('=> ' + i + ' records\n');
+});
+
+
