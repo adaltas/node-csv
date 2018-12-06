099971c9d73ed7c01b71c96f0b8823731dda4979
David Worms <david@adaltas.com>
2018-12-06 01:04:45 +0100

    chore(csv-stringify): delimiter: new samples

---
+++ samples/option.delimiter_multiple.js
@@ -0,0 +1,12 @@
+
+const stringify = require('../lib')
+const assert = require('assert')
+
+stringify([
+  ['1', '2'],
+  ['3', '4']
+], {
+  delimiter: ':)'
+}, function(err, records){
+  assert.equal(records, '1:)2\n3:)4\n')
+})
