a5b6a6039abdd634692de1885b04e1dd383b861a
David Worms <david@adaltas.com>
2019-04-26 00:22:58 +0200

    chore(stream-transform): samples: reflext state isolation

--- samples/state.instance.js
+++ samples/state.instance.js
@@ -5,14 +5,14 @@ const records = 'record\n'.repeat(5).trim().split('\n')
 // Initialize the transformation
 const transformer = transform(records, (record, callback) => {
   setTimeout( () => {
-    const {running, started, finished} = transformer
+    const {running, started, finished} = transformer.state
     callback(null, `${running}_${started}_${finished}\n`)
   }, 100)
 })
 // Get notify when done
 transformer.on('end', () => {
   process.stdout.write('-------\n')
-  const {running, started, finished} = transformer
+  const {running, started, finished} = transformer.state
   process.stdout.write(`${running}_${started}_${finished}\n`)
 })
 // Print the transformed records to the standard output
