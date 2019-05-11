b1576babecd0d3e9855c7aff9a97409eb5240904
Florian Keller <github@floriankeller.de>
2019-05-11 19:47:11 +0200

    fix types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,16 +1,13 @@
-
 // Alias to the ES6 modules exposing the stream and callback APIs
 
-import * as generate from "csv-generate/lib/index";
-import * as parse from "csv-parse/lib/index";
-import * as transform from "stream-transform/lib/index";
-import * as stringify from "csv-stringify/lib/index";
+import * as generateImport from 'csv-generate';
+import * as parseImport from 'csv-parse';
+import * as transformImport from 'stream-transform';
+import * as stringifyImport from 'csv-stringify';
 
-export csv;
+export var generate: typeof generateImport;
+export var parse: typeof parseImport;
+export var transform: typeof transformImport;
+export var stringify: typeof stringifyImport;
 
-interface csv {
-  generate: generate,
-  parse: parse,
-  transform: transform,
-  stringify: stringify,
-}
+export as namespace csv;
