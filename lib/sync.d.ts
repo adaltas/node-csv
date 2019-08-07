9c531992ddeb24a09e5086629c185a0b0ae4cde3
Rauno Viskus <Rauno56@gmail.com>
2019-08-08 11:18:19 +1200

    fix: add buffer as allowed type for input

--- lib/sync.d.ts
+++ lib/sync.d.ts
@@ -2,5 +2,5 @@ import * as csvParse from './index';
 
 export = parse;
 
-declare function parse(input: string, options?: csvParse.Options): any;
+declare function parse(input: Buffer | string, options?: csvParse.Options): any;
 declare namespace parse {}
