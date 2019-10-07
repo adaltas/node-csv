6704eab1adcca6e9e93dfd7647f3aaf6a641e88f
David Worms <david@adaltas.com>
2019-10-07 09:42:29 +0200

    sample: refactor async iterator

--- samples/async.iterator.js
+++ samples/async.iterator.js
@@ -1,14 +1,14 @@
 
-const assert = require('assert')
-const parse = require('..')
-const generate = require('csv-generate')
-
-const main = async function(){
-  // Initialise the parser by generating random records
-  const parser = generate({
+(async () => {
+  const assert = require('assert')
+  const parse = require('..')
+  const generate = require('csv-generate')
+  // Create a readable input stream, could be `fs.createReadStream`
+  const input = generate({
     length: 10000
   })
-  .pipe(parse())
+  // Initialise the parser by generating random records
+  const parser = input.pipe(parse())
   // Iterate through each records
   let count = 0
   for await (const record of parser) {
@@ -16,5 +16,4 @@ const main = async function(){
   }
   // Validation
   assert.strictEqual(count, 10000)
-}
-main()
+})();
