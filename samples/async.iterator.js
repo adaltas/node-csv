7b69213ad4ec0d1ccd3b534df06867d113b9b90f
David Worms <david@adaltas.com>
2020-02-07 15:09:44 +0100

    chore(csv-parse): async: update iterator sample

--- samples/async.iterator.js
+++ samples/async.iterator.js
@@ -1,19 +1,38 @@
 
+const assert = require('assert');
+const generate = require('csv-generate');
+const parse = require('..');
+
+// Parameters
+const wait = 100;
+let count = 0;
+
 (async () => {
-  const assert = require('assert')
-  const parse = require('..')
-  const generate = require('csv-generate')
-  // Create a readable input stream, could be `fs.createReadStream`
-  const input = generate({
-    length: 10000
-  })
   // Initialise the parser by generating random records
-  const parser = input.pipe(parse())
+  const parser = generate({
+    high_water_mark: 64 * 64,
+    length: 1000
+  }).pipe(
+    parse()
+  )
+  // Report start
+  process.stdout.write('start\n')
   // Iterate through each records
-  let count = 0
   for await (const record of parser) {
     count++
+    // Report current line
+    process.stdout.write(`${count} ${record.join(',')}\n`)
+    // Fake asynchronous operation
+    await write()
   }
+  // Report end
+  process.stdout.write('...done\n')
   // Validation
   assert.strictEqual(count, 10000)
-})();
+})()
+// A fake asynchronous write function
+const write = function(){
+  return new Promise((resolve) => setTimeout(function(){
+    resolve()
+  }, 100))
+};
