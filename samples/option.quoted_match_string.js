ca4b0e7c4f2c85ad86b763f665f119601c5d60a9
David Worms <david@adaltas.com>
2020-04-21 22:37:56 +0200

    chore(csv-stringify): samples: add quoted examples

---
+++ samples/option.quoted_match_string.js
@@ -0,0 +1,11 @@
+
+const stringify = require('../lib')
+const assert = require('assert')
+
+stringify([
+  ['a value', '.', 'value.with.dot'],
+], {
+  quoted_match: '.'
+}, function(err, records){
+  assert.equal(records, 'a value,".","value.with.dot"\n')
+})
