0e94b34c8bf8baaed3a1e28ce35ab99360696624
David Worms <david@adaltas.com>
2021-04-03 22:52:43 +0200

    docs: rewrite trim sample

--- samples/option.trim.js
+++ samples/option.trim.js
@@ -2,11 +2,7 @@
 const parse = require('../lib/sync')
 const assert = require('assert')
 
-const records = parse(`
-a ,1
-b, 2
- c,3
-`.trim(), {
+const records = parse('a ,1\nb, 2\n c,3', {
   trim: true
 })
 assert.deepStrictEqual(
