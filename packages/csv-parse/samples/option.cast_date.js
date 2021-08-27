8222676103a9ad685480546f53ec1bf6a0bb9ea8
David Worms <david@adaltas.com>
2021-08-27 21:52:55 +0200

    docs(csv-parse): new cast_date sample

---
+++ packages/csv-parse/samples/option.cast_date.js
@@ -0,0 +1,16 @@
+
+const parse = require('../lib/sync')
+const assert = require('assert')
+
+const data = `
+2000-01-01,date1
+2020-01-01,date2
+`.trim()
+const records = parse(data, {
+  cast: true,
+  cast_date: true
+})
+assert.deepStrictEqual(records, [
+  [ new Date('2000-01-01T00:00:00.000Z'), 'date1' ],
+  [ new Date('2020-01-01T00:00:00.000Z'), 'date2' ]
+])
