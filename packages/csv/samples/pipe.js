a5b4cf506c678eab089b868373427608a68514bb
David Worms <david@adaltas.com>
2021-10-18 23:16:32 +0200

    docs(csv): update pipe comments

--- packages/csv/samples/pipe.js
+++ packages/csv/samples/pipe.js
@@ -1,24 +1,25 @@
 
 // Import the package main module
 import * as csv from '../lib/index.js'
-// Use the module
+
+// Run the pipeline
 csv
 // Generate 20 records
 .generate({
   delimiter: '|',
   length: 20
 })
-// Parse the records
+// Transform CSV data into records
 .pipe(csv.parse({
   delimiter: '|'
 }))
 // Transform each value into uppercase
-.pipe(csv.transform(function(record){
-   return record.map(function(value){
+.pipe(csv.transform((record) => {
+   return record.map((value) => {
      return value.toUpperCase()
    });
 }))
-// Convert the object into a stream
+// Convert objects into a stream
 .pipe(csv.stringify({
   quoted: true
 }))
