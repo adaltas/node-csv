089ae5f88cd6d3b0d390532e44f9a20651700252
David Worms <david@adaltas.com>
2021-10-26 21:44:32 +0200

    docs(csv-parse): file utf16le sample

---
+++ packages/csv-parse/samples/recipe.file.utf16le.js
@@ -0,0 +1,25 @@
+
+import assert from 'assert';
+import fs from 'fs/promises';
+import os from 'os';
+import { parse } from '../lib/sync.js';
+
+// Prepare the dataset
+await fs.writeFile(`${os.tmpdir()}/input.csv`, Buffer.from([
+  '\ufeff',
+  `a€b€c`,
+  '\n',
+  `d€e€f`
+].join(''), 'utf16le'));
+// Read the content
+const content = await fs.readFile(`${os.tmpdir()}/input.csv`);
+// Parse the CSV content
+const records = parse(content, {
+  delimiter: '€',
+  encoding: 'utf16le'
+});
+// Validate the records
+assert.deepStrictEqual(records, [
+  [ '﻿a', 'b', 'c' ],
+  [ 'd', 'e', 'f' ]
+])
