13bb7826fe2fe0d9ef8746c2626b27db367cd025
David Worms <david@adaltas.com>
2021-10-26 16:57:33 +0200

    docs(csv-parse): new delimiter array sample

---
+++ packages/csv-parse/samples/option.delimiter.array.js
@@ -0,0 +1,36 @@
+
+import assert from 'assert';
+import { parse } from 'csv-parse/sync';
+
+const input = `color name::red::green::blue
+Cyan \t     "0"   :: 255   :: 255
+Yellow \t   "255" :: "255" ::"0"
+Hot Pink \t "255" :: 105   :: "180"`
+
+const output = parse(input, {
+  delimiter: ["::","\t"],
+  trim: true,
+  columns: true,
+}).map((rec,index) => {
+  let indent = ""
+  return Object.entries(rec).map(([key, value]) => {
+    const row = `${indent}${key}: <${value}>`;
+    indent = (indent.length === 0 ? "    " : indent)
+    return row
+  }).join('\n')
+}).join('\n')
+
+assert.equal( output, `
+color name: <Cyan>
+    red: <0>
+    green: <255>
+    blue: <255>
+color name: <Yellow>
+    red: <255>
+    green: <255>
+    blue: <0>
+color name: <Hot Pink>
+    red: <255>
+    green: <105>
+    blue: <180>
+`.trim())
