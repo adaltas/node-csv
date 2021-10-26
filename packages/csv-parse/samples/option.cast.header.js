3f883123bdcfc41a01674f02ddff47ed6e83db95
David Worms <david@adaltas.com>
2021-10-26 11:35:02 +0200

    docs: issue 298 file 2

---
+++ packages/csv-parse/samples/option.cast.header.js
@@ -0,0 +1,20 @@
+
+import {parse} from 'csv-parse/sync';
+import assert from 'assert';
+
+assert.deepEqual(
+  parse('a,b,c\n1,2,3\n4,5,6', {
+    cast: (value, context) => {
+      if(context.header) return value;
+      if (context.column === 'B') return Number(value);
+      return String(value);
+    },
+    columns: (header) => {
+      return header.map((label) => label.toUpperCase());
+    },
+    trim: true,
+  })
+, [
+  { A: '1', B: 2, C: '3' },
+  { A: '4', B: 5, C: '6' }
+]);
