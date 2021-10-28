2481158905af8d3c23caad39bd4af5fdfb4ee607
David Worms <david@adaltas.com>
2021-10-28 16:12:13 +0200

    docs(csv): new sync sample

---
+++ packages/csv/samples/sync.js
@@ -0,0 +1,19 @@
+
+import assert from 'assert';
+import {generate, parse, transform, stringify} from 'csv/sync';
+
+// Run the pipeline
+const input = generate({seed: 1, columns: 2, length: 5});
+const rawRecords = parse(input);
+const refinedRecords = transform(rawRecords, function(data){
+  return data.map(function(value){return value.toUpperCase();});
+});
+const output = stringify(refinedRecords);
+// Print the final result
+assert.equal(output, 
+  `OMH,ONKCHHJMJADOA
+D,GEACHIN
+NNMIN,CGFDKB
+NIL,JNNMJADNMINL
+KB,DMIM
+`);
