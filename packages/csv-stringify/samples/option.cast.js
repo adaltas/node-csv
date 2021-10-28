3bc31c9ba35e283d4e220138341193e246645c1c
David Worms <david@adaltas.com>
2021-10-28 17:29:45 +0200

    docs(csv-stringify): new cast option sample

--- packages/csv-stringify/samples/option.cast.js
+++ packages/csv-stringify/samples/option.cast.js
@@ -1,23 +1,13 @@
 
-import { stringify } from 'csv-stringify';
 import assert from 'assert';
+import { stringify } from 'csv-stringify/sync';
 
-stringify([{
-  name: 'foo',
-  date: new Date(1970, 0)
-},{
-  name: 'bar',
-  date: new Date(1971, 0)
-}],{
+const data = stringify( [ [1], [2] ], {
   cast: {
-    date: function(value) {
-      return value.toISOString();
+    number: function(value){
+      return {value: `="${value}"`, quote: false}
     }
   }
-}, function(err, data) {
-  assert.equal(
-    data,
-    "foo,1969-12-31T23:00:00.000Z\n" +
-    "bar,1970-12-31T23:00:00.000Z\n"
-  );
-});
+})
+
+assert.equal(data, '="1"\n="2"\n')
