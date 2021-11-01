997a363cae004e9286dd8c0991c39354185aef9e
David Worms <david@adaltas.com>
2021-11-01 11:13:28 +0100

    docs(csv-generate): new mixed api sample

---
+++ packages/stream-transform/samples/api.mixed.input.stream.js
@@ -0,0 +1,19 @@
+
+import { transform } from 'stream-transform';
+import assert from 'assert';
+
+// Create the parser
+const transformer = transform(function(record){
+  record.push(record.shift());
+  return record;
+}, function(err, output){
+  assert.deepEqual(output, [
+    [ '2', '3', '4', '1' ],
+    [ 'b', 'c', 'd', 'a' ]
+  ]);
+});
+// Write data to the stream
+transformer.write(['1','2','3','4']);
+transformer.write(['a','b','c','d']);
+// Close the readable stream
+transformer.end();
