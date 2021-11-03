b37395ede83b7854a4f8a8f98eef8e5d98264f1c
David Worms <david@adaltas.com>
2021-11-03 16:58:12 +0100

    docs: move csv-parse browser into demo

--- demo/browser/index.js
+++ demo/browser/index.js
@@ -7,8 +7,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+// const parse = await import.meta.resolve('csv-parse/package.json')
+// console.log(parse);
+
 app.use(express.static(__dirname))
-app.use('/lib', express.static(`${__dirname}/../../dist/iife`))
+app.use('/lib/parse/', express.static(`${__dirname}/../../packages/csv-parse/dist/iife/`))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
