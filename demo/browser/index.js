4dbd781f284a8e664979ca345a77d092868173ca
David Worms <david@adaltas.com>
2021-11-03 17:37:08 +0100

    docs: demo for generate, stringify, transform

--- demo/browser/index.js
+++ demo/browser/index.js
@@ -10,9 +10,12 @@ const port = 3000;
 // const parse = await import.meta.resolve('csv-parse/package.json')
 // console.log(parse);
 
-app.use(express.static(__dirname))
-app.use('/lib/parse/', express.static(`${__dirname}/../../packages/csv-parse/dist/iife/`))
+app.use(express.static(__dirname));
+app.use('/lib/generate/', express.static(`${__dirname}/../../packages/csv-generate/dist/iife/`));
+app.use('/lib/parse/', express.static(`${__dirname}/../../packages/csv-parse/dist/iife/`));
+app.use('/lib/transform/', express.static(`${__dirname}/../../packages/stream-transform/dist/iife/`));
+app.use('/lib/stringify/', express.static(`${__dirname}/../../packages/csv-stringify/dist/iife/`));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
