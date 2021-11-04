ba29edb79b13b31d78fba1e54e87e84807e485b1
David Worms <david@adaltas.com>
2021-11-04 22:02:27 +0100

    docs: demo instructions to esm script

--- demo/esm/test.sh
+++ demo/esm/test.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-node --loader ts-node/esm lib/parse.ts
+version=`node -v`
+
+if [[ $version =~ ^v12\.16\..* ]]; then
+  node --experimental-modules lib/csv.js
+  node --experimental-modules lib/parse.js
+else # starting with Node.js 12.17.0
+  node lib/parse.js
+fi
+
+# node --loader ts-node/esm lib/parse.ts
