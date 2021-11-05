5a3aeff644aa20f70f31ff4e61727061ba56450d
David Worms <david@adaltas.com>
2021-11-06 00:43:02 +0100

    docs(csv-parse): new objname index sample

---
+++ packages/csv-parse/samples/option.objname.column.js
@@ -0,0 +1,13 @@
+
+import assert from 'assert';
+import { parse } from 'csv-parse/sync';
+
+const records = parse('c1,c2,c3\na,b,c\nd,e,f', {
+  columns: true,
+  objname: 'c2'
+});
+
+assert.deepStrictEqual(records, {
+  b: { c1: 'a', c2: 'b', c3: 'c' },
+  e: { c1: 'd', c2: 'e', c3: 'f' } 
+});
---
+++ packages/csv-parse/samples/option.objname.index.js
@@ -0,0 +1,12 @@
+
+import assert from 'assert';
+import { parse } from 'csv-parse/sync';
+
+const records = parse('a,b,c\nd,e,f', {
+  objname: 1
+});
+
+assert.deepStrictEqual(records, {
+  b: ['a', 'b', 'c'],
+  e: ['d', 'e', 'f']
+});
