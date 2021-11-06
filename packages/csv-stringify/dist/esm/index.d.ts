cc30d66e0f07686d2c42670ead10246ebcf37a67
David Worms <david@adaltas.com>
2021-11-06 16:01:03 +0100

    feat(csv-stringify): ts extends options with stream.TransformOptions (#301)

--- packages/csv-stringify/dist/esm/index.d.ts
+++ packages/csv-stringify/dist/esm/index.d.ts
@@ -17,7 +17,7 @@ export interface CastingContext {
     readonly index: number;
     readonly records: number;
 }
-export interface Options {
+export interface Options extends stream.TransformOptions {
     /**
      * Prepend the byte order mark (BOM) to the output stream.
      */
