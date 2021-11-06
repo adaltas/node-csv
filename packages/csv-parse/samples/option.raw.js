67053ab6497682e61f95ed970cb216ebd8ee0c09
David Worms <david@adaltas.com>
2021-11-06 17:34:36 +0100

    docs(csv-parse): raw sample

---
+++ packages/csv-parse/samples/option.raw.js
@@ -0,0 +1,13 @@
+
+import assert from 'assert';
+import { parse } from 'csv-parse';
+
+parse(`
+a,b,c
+d,e,f
+`.trim(), {raw: true}, (err, records) => {
+  assert.deepStrictEqual(records, [
+    { record: [ 'a', 'b', 'c' ], raw: 'a,b,c\n' },
+    { record: [ 'd', 'e', 'f' ], raw: 'd,e,f' }
+  ])
+})
