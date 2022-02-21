4958529c229ff0188a187c15e76e32f0adfe43c3
David Worms <david@adaltas.com>
2022-02-21 16:21:27 +0100

    test: coffee esm loader options

--- packages/stream-transform/test/loaders/coffee.js
+++ packages/stream-transform/test/loaders/coffee.js
@@ -1,12 +1,19 @@
 import CoffeeScript from 'coffeescript';
 
-const coffeeRegex = /\.coffee$|\.litcoffee$|\.coffee\.md$/;
+// See https://github.com/nodejs/node/issues/36396
+const extensionsRegex = /\.coffee$|\.litcoffee$|\.coffee\.md$/;
 
 export async function load(url, context, next) {
-  if (coffeeRegex.test(url)) {
+  if (extensionsRegex.test(url)) {
     const format = 'module';
     const { source: rawSource } = await next(url, { format });
-    const source = CoffeeScript.compile(rawSource.toString(), { bare: true });
+    const source = CoffeeScript.compile(rawSource.toString(), {
+      bare: true,
+      inlineMap: true,
+      filename: url,
+      header: false,
+      sourceMap: false,
+    });
     return {format, source};
   }
   return next(url, context);
