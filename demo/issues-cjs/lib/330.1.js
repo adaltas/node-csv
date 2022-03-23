3d85a411007416f3cb750ca6b427f55c0331a8b8
David Worms <david@adaltas.com>
2022-03-23 09:37:47 +0100

    feat(csv-issues-cjs): 330 sample code

---
+++ demo/issues-cjs/lib/330.1.js
@@ -0,0 +1,13 @@
+const {parse} = require('csv/sync');
+
+const input = `
+Date, Type, Description, Value, Balance, Account Name, Account Number
+
+11/02/2022,BAC,"'FOO",10.00,33432.80,"'REF","'123123-12312312",
+`;
+
+const output = parse(input, {
+  skip_empty_lines: true,
+  relax_column_count: true
+});
+console.log(output);
