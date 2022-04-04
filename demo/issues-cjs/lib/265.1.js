f79fe219577b3516d662df62f8802f8714c4abdb
David Worms <david@adaltas.com>
2022-04-04 09:17:12 +0200

    docs(csv-issues-cjs): 265 sample

---
+++ demo/issues-cjs/lib/265.1.js
@@ -0,0 +1,14 @@
+
+const {parse} = require('csv-parse');
+const fs = require('fs');
+
+fs.createReadStream(`${__dirname}/265.tubs.csv`)
+.pipe(parse({
+  delimiter: ',',
+  columns: false,
+  skip_empty_lines: true,
+  trim: true,
+}))
+.on('data', data => {
+    console.log(data);
+});
