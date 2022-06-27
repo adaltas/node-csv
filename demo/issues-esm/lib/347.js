bfc44a0a9ef9c5b60d836467e6e59e1b9a078747
David Worms <david@adaltas.com>
2022-06-27 16:53:47 +0200

    docs(csv-issues-esm): prepend data to stream (fix #347)

---
+++ demo/issues-esm/lib/347.js
@@ -0,0 +1,27 @@
+
+import fs from 'fs'
+import { stringify } from 'csv-stringify';
+import { generate } from 'csv-generate';
+
+// Write stream initialisation
+const ws = fs.createWriteStream('/tmp/a_test')
+// Write some metadata
+ws.write('---\n')
+ws.write('propery: My Value\n')
+ws.write('---\n')
+
+generate({
+  length: 20,
+  objectMode: true,
+  seed: 1,
+  headers: 2,
+  duration: 400
+})
+.pipe(stringify({
+  header: true,
+  columns: {
+    year: 'birthYear',
+    phone: 'phone'
+  }
+}))
+.pipe(ws);
