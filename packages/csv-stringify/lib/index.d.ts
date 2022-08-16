7a528bf21af3425912e4a7fcdca8e0c66275ed19
Benoit Lemoine <lemoine.benoit@gmail.com>
2022-08-16 13:19:18 -0400

    feat(ts): set `columns` type as `readonly`
    The goal is for stringify to support values declared `as const`. 
    
    eg.
    ```
    const columns = ['name', 'age'] as const
    
    stringify({columns})
    ```
    
    As this array has no reasons to be mutated by `stringify` it's simpler to set it as `readonly`.

--- packages/csv-stringify/lib/index.d.ts
+++ packages/csv-stringify/lib/index.d.ts
@@ -60,7 +60,7 @@ export interface Options extends stream.TransformOptions {
      * can refer to nested properties of the input JSON
      * see the "header" option on how to print columns names on the first line
      */
-    columns?: string[] | PlainObject<string> | ColumnOption[]
+    columns?: readonly string[] | PlainObject<string> | readonly ColumnOption[]
     /**
      * Set the field delimiter, one character only, defaults to a comma.
      */
