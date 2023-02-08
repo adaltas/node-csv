fa09d03aaf0008b2790656871ca6b2c4be12d14c
David Worms <david@adaltas.com>
2023-02-08 09:46:28 +0100

    fix: support ts node16 resolution in cjs (#354)

---
+++ demo/ts-cjs-node16/lib/index.ts
@@ -0,0 +1,28 @@
+
+import assert from 'assert'
+import { generate, Generator } from 'csv-generate';
+import { parse, Parser } from 'csv-parse';
+import { transform, Transformer } from 'stream-transform';
+import { stringify, Stringifier } from 'csv-stringify';
+
+let chunks: string[] = [];
+
+// Create the parser
+const generator: Generator = generate({length: 2, seed: true});
+const parser: Parser = parse();
+const transformer: Transformer = transform((record) => record);
+const stringifier: Stringifier = stringify();
+generator.pipe(parser).pipe(transformer).pipe(stringifier)
+  .on('data', function(data) {
+    chunks.push(data.toString())
+  })
+  .on('end', () => {
+    assert.strictEqual(
+      chunks.join(''),
+      [
+        'OMH,ONKCHhJmjadoA,D,GeACHiN,nnmiN,CGfDKB,NIl,JnnmjadnmiNL',
+        'KB,dmiM,fENL,Jn,opEMIkdmiOMFckep,MIj,bgIjadnn,fENLEOMIkbhLDK',
+        '',
+      ].join('\n')
+    )
+  })
