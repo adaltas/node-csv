29d6b6e152f0724cd685dce8938d53a0f4642f92
David Worms <david@adaltas.com>
2023-03-01 22:54:20 +0100

    docs(csv-stringify): iterator sample

---
+++ packages/csv-stringify/samples/api.async.iterator.js
@@ -0,0 +1,30 @@
+
+import assert from 'assert';
+import { generate } from 'csv-generate';
+import { stringify } from 'csv-stringify';
+
+(async () => {
+  // Initialise the parser by generating random records
+  const parser = generate({
+    length: 1000,
+    objectMode: true,
+    seed: true
+  }).pipe(
+    stringify()
+  );
+  // Intialise count
+  let count = 0;
+  // Report start
+  process.stdout.write('start\n');
+  // Iterate through each records
+  for await (const row of parser) {
+    // Report current line
+    process.stdout.write(`${count++} ${row}\n`);
+    // Fake asynchronous operation
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+  // Report end
+  process.stdout.write('...done\n');
+  // Validation
+  assert.strictEqual(count, 5);
+})();
