48c6dbca91b5656954c5800704f65148997be5a9
David Worms <david@adaltas.com>
2023-03-01 22:36:43 +0100

    docs(csv-stringify): mixed examples

---
+++ packages/csv-stringify/samples/mixed.input_stream.js
@@ -0,0 +1,19 @@
+
+import assert from 'assert';
+import { stringify } from 'csv-stringify';
+
+// Create the parser
+const stringifier = stringify({
+  delimiter: ':'
+}, function(err, data){
+  assert.deepStrictEqual(
+    data,
+    'root:x:0:0:root:/root:/bin/bash\n' +
+    'someone:x:1022:1022::/home/someone:/bin/bash\n'
+  );
+});
+// Write records to the stream
+stringifier.write([ 'root','x','0','0','root','/root','/bin/bash' ]);
+stringifier.write([ 'someone','x','1022','1022','','/home/someone','/bin/bash' ]);
+// Close the writable stream
+stringifier.end();
---
+++ packages/csv-stringify/samples/mixed.output_stream.js
@@ -0,0 +1,22 @@
+
+import assert from 'assert';
+import { stringify } from 'csv-stringify';
+
+const data = [];
+stringify([
+  [ '1', '2', '3', '4' ],
+  [ 'a', 'b', 'c', 'd' ]
+])
+// Use the readable stream api
+  .on('readable', function(){
+    let row; while ((row = this.read()) !== null) {
+      data.push(row);
+    }
+  })
+// When we are done, test that the parsed records matched what expected
+  .on('end', function(){
+    assert.deepStrictEqual(
+      data.join(''),
+      '1,2,3,4\na,b,c,d\n'
+    );
+  });
