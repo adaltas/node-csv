5ad98c4ff48a9af87662fafdf2f89a176d8dadee
David Worms <david@adaltas.com>
2023-08-25 16:54:37 +0200

    docs(csv-parse): comment_no_infix sample

---
+++ packages/csv-parse/samples/option.comment_no_infix.js
@@ -0,0 +1,15 @@
+
+import assert from 'assert';
+import { parse } from 'csv-parse/sync';
+
+const output = parse(`
+# Illustrate the usage of comment_no_infix
+a,b#,c
+`.trim(), {
+  comment: '#',
+  comment_no_infix: true
+});
+
+assert.deepStrictEqual(output, [
+  ['a', 'b#', 'c']
+]);
