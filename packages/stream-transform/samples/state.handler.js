07c314e17825c7736e073d2e0488e09785915b73
David Worms <david@adaltas.com>
2023-10-04 14:10:48 +0200

    docs(stream-transform): update state handler sample

--- packages/stream-transform/samples/state.handler.js
+++ packages/stream-transform/samples/state.handler.js
@@ -4,24 +4,26 @@ import assert from 'assert';
 
 // Generate a dataset of 5 records
 const records = 'record\n'.repeat(5).trim().split('\n');
-let test_running = records.length;
-const test_started = records.length;
-let test_finished = 0;
+const assertions = [
+  '2_2_0',
+  '1_2_1',
+  '2_4_2',
+  '1_4_3',
+  '1_5_4',
+];
+let count = 0;
 // Execute the transformation
-transform(records, function(_, callback){
+transform(records, {parallel: 2}, function(_, callback){
   setTimeout(() => {
     const {running, started, finished} = this.state;
-    assert.equal(running, test_running--);
-    assert.equal(started, test_started);
-    assert.equal(finished, test_finished++);
+    assert.equal(assertions[count++], `${running}_${started}_${finished}`);
     callback(null, `${running}_${started}_${finished}\n`);
   }, 100);
 })
   // Get notify on error
   .on('end', function(){
-    process.stdout.write('-------\n');
     const {running, started, finished} = this.state;
-    process.stdout.write(`${running}_${started}_${finished}\n`);
+    process.stdout.write(`end: ${running}_${started}_${finished}\n`);
   })
   // Print the transformed records to the standard output
   .pipe(process.stdout);
