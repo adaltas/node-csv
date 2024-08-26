400dd2ef376294e859640f1376e6bd679483ac2b
David Worms <david@adaltas.com>
2024-08-26 08:44:11 +0200

    test: eslint in loaders

--- packages/csv-stringify/test/loaders/coffee.js
+++ packages/csv-stringify/test/loaders/coffee.js
@@ -1,11 +1,11 @@
-import CoffeeScript from 'coffeescript';
+import CoffeeScript from "coffeescript";
 
 // See https://github.com/nodejs/node/issues/36396
 const extensionsRegex = /\.coffee$|\.litcoffee$|\.coffee\.md$/;
 
 export async function load(url, context, next) {
   if (extensionsRegex.test(url)) {
-    const format = 'module';
+    const format = "module";
     const { source: rawSource } = await next(url, { format });
     const source = CoffeeScript.compile(rawSource.toString(), {
       bare: true,
@@ -14,7 +14,7 @@ export async function load(url, context, next) {
       header: false,
       sourceMap: false,
     });
-    return {format, source};
+    return { format, source };
   }
   return next(url, context);
 }
