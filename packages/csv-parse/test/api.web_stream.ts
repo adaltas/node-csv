d33a61a8a575f8a48545fc7e20fc56221ad339dc
David Worms <david@adaltas.com>
2024-11-20 10:14:52 +0100

    test(csv-parse): error thrown by webstream

--- packages/csv-parse/test/api.web_stream.ts
+++ packages/csv-parse/test/api.web_stream.ts
@@ -1,5 +1,6 @@
-import assert from 'assert';
+import 'should'
 import {parse as parseStream} from '../lib/stream.js'
+import { CsvError } from '../lib/index.js'
 
 describe('API Web Stream', () => {
   
@@ -7,25 +8,38 @@ describe('API Web Stream', () => {
     
     it('simple parse', async () => {
       const stream = parseStream();
-
       const writer = stream.writable.getWriter();
       const reader = stream.readable.getReader();
-      
       await writer.write(Buffer.from("A,B,C\nD,E,F"));
       await writer.close();
-      
-      assert.deepStrictEqual(await reader.read(), {
+      await reader.read().should.finally.eql({
         done: false,
         value: ['A', 'B', 'C'],
       });
-      assert.deepStrictEqual(await reader.read(), {
+      await reader.read().should.finally.eql({
         done: false,
         value: ['D', 'E', 'F'],
       });
-      assert.deepStrictEqual(await reader.read(), {
+      await reader.read().should.finally.eql({
         done: true,
         value: undefined,
       });
     })
+    
+    it("cat error parse", async function () {
+      const stream = parseStream();
+      const writer = stream.writable.getWriter();
+      try {
+        await writer.write(Buffer.from("A,B,C\nD,E"));
+        await writer.close();
+        throw Error("Shall not be called");
+      } catch (err) {
+        if (!(err instanceof CsvError)) {
+          throw Error("Invalid error type");
+        }
+        err.code.should.eql("CSV_RECORD_INCONSISTENT_FIELDS_LENGTH");
+      }
+    });
+
   })
 })
