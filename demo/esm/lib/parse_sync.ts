5ad1f2db3229867d0e2f58f056752e65e4e37102
David Worms <david@adaltas.com>
2025-07-10 12:29:03 +0200

    fix(csv-demo-esm): ts type

--- demo/esm/lib/parse_sync.ts
+++ demo/esm/lib/parse_sync.ts
@@ -2,7 +2,7 @@ import assert from "assert";
 import { parse } from "csv-parse/sync";
 
 // Create the parser
-const records: [] = parse(["a:b:c\n", "1:2:3\n"].join(""), {
+const records: string[][] = parse(["a:b:c\n", "1:2:3\n"].join(""), {
   delimiter: ":",
 });
 // Test that the parsed records matched what's expected
