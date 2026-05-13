eabdf77b5d596d8344465dfa10fdebbd25df27ea
David Worms <david@adaltas.com>
2026-05-13 09:53:53 +0200

    refactor(csv-stringify): remove eslint warnings

--- packages/csv-stringify/test/option.cast.null.ts
+++ packages/csv-stringify/test/option.cast.null.ts
@@ -1,57 +1,50 @@
 import "should";
-import dedent from "dedent";
 import { stringify } from "../lib/index.js";
 
 describe("Option `cast` with null and undefined", function () {
   it("cast.null can emit unquoted NULL", function (next) {
     stringify(
-      [
-        { a: "foo", b: null, c: "bar" }
-      ],
+      [{ a: "foo", b: null, c: "bar" }],
       {
         header: true,
         cast: {
-          null: function (value, context) {
+          null: function () {
             return { value: "NULL", quoted: false };
-          }
-        }
+          },
+        },
       },
       (err, data) => {
         if (!err) {
           data.should.eql("a,b,c\nfoo,NULL,bar\n");
         }
         next(err);
-      }
+      },
     );
   });
 
   it("cast.undefined can emit unquoted NULL", function (next) {
     stringify(
-      [
-        { a: "foo", b: undefined, c: "bar" }
-      ],
+      [{ a: "foo", b: undefined, c: "bar" }],
       {
         header: true,
         cast: {
-          undefined: function (value, context) {
+          undefined: function () {
             return { value: "NULL", quoted: false };
-          }
-        }
+          },
+        },
       },
       (err, data) => {
         if (!err) {
           data.should.eql("a,b,c\nfoo,NULL,bar\n");
         }
         next(err);
-      }
+      },
     );
   });
 
   it("cast.null receives expected context", function (next) {
     stringify(
-      [
-        [null]
-      ],
+      [[null]],
       {
         cast: {
           null: function (value, context) {
@@ -59,52 +52,48 @@ describe("Option `cast` with null and undefined", function () {
               .sort()
               .should.eql(["column", "header", "index", "records"]);
             return "";
-          }
-        }
+          },
+        },
       },
-      next
+      next,
     );
   });
 
   it("cast.null returning a string respects quoting rules", function (next) {
     stringify(
-      [
-        { a: "foo", b: null, c: "bar" }
-      ],
+      [{ a: "foo", b: null, c: "bar" }],
       {
         quoted: true,
         cast: {
-          null: function (value, context) {
+          null: function () {
             return "NULL";
-          }
-        }
+          },
+        },
       },
       (err, data) => {
         if (!err) {
           data.should.eql('"foo","NULL","bar"\n');
         }
         next(err);
-      }
+      },
     );
   });
 
   it("cast.null catches error and surfaces it", function (next) {
     stringify(
-      [
-        [null]
-      ],
+      [[null]],
       {
         cast: {
           null: function () {
             throw Error("Catchme");
-          }
-        }
+          },
+        },
       },
       (err) => {
         if (!err) return next(Error("Invalid assessment"));
         err.message.should.eql("Catchme");
         next();
-      }
+      },
     );
   });
 });
