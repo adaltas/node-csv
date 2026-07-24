46050e300d5b2868b3c5fae97cae166ba96f8a71
Campbell <campbell.wass@gmail.com>
2026-07-24 16:41:16 +1000

    perf(csv-parse): avoid unnecessary allocation in ResizeableBuffer.toString

--- packages/csv-parse/lib/utils/ResizeableBuffer.js
+++ packages/csv-parse/lib/utils/ResizeableBuffer.js
@@ -47,7 +47,7 @@ class ResizeableBuffer {
   }
   toString(encoding) {
     if (encoding) {
-      return this.buf.slice(0, this.length).toString(encoding);
+      return this.buf.toString(encoding, 0, this.length);
     } else {
       return Uint8Array.prototype.slice.call(this.buf.slice(0, this.length));
     }
